75b08abffae59f8b37bafb0732d8bfed6d1803c8
jason-ibisi <jasonibisi@gmail.com>
2022-07-25 10:44:54 +0100

    refactor: updated environment variable for frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost:
+    'http://aea2bd204bf0347cb9476fdf772e96f8-152635462.us-east-1.elb.amazonaws.com:8080/api/v0',
 };
 
 /*
